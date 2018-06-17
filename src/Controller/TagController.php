c0cdd10b9a3f3f136581b4f3d97d92239e35a1dc
Kishlin <xd3Rk703pThd9>
2018-06-17 17:31:08 +0200

    Added monolog.

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Tag;
 use App\Form\TagType;
 use App\Repository\TagRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,22 +18,36 @@ class TagController extends Controller
 {
     /**
      * @Route("/", name="tag_index", methods="GET")
+     * @param TagRepository $tagRepository
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function index(TagRepository $tagRepository): Response
+    public function index(TagRepository $tagRepository, LoggerInterface $logger): Response
     {
+        $logger->debug('Returning tag entities.', array('method' => 'index', 'class' => self::class));
         return $this->render('tag/index.html.twig', ['tags' => $tagRepository->findAll()]);
     }
 
     /**
      * @Route("/new", name="tag_new", methods="GET|POST")
+     * @param Request $request
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function new(Request $request): Response
+    public function new(Request $request, LoggerInterface $logger): Response
     {
+        $logger->debug('Request for a new tag entity.', array('method' => 'new', 'class' => self::class));
+
         $tag = new Tag();
         $form = $this->createForm(TagType::class, $tag);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $logger->debug(
+                'Form is submitted and valid.',
+                array('tag' => $tag->getLabel(), 'method' => 'new', 'class' => self::class)
+            );
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($tag);
             $em->flush();
@@ -40,6 +55,7 @@ class TagController extends Controller
             return $this->redirectToRoute('tag_index');
         }
 
+        $logger->debug('Form is not submitted. Rendering template.', array('method' => 'new', 'class' => self::class));
         return $this->render('tag/new.html.twig', [
             'tag' => $tag,
             'form' => $form->createView(),
@@ -48,25 +64,52 @@ class TagController extends Controller
 
     /**
      * @Route("/{id}", name="tag_show", methods="GET")
+     * @param Tag $tag
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function show(Tag $tag): Response
+    public function show(Tag $tag, LoggerInterface $logger): Response
     {
+        $logger->debug(
+            'Showing tag from given id.',
+            array('tag' => $tag->getLabel(), 'method' => 'show', 'class' => self::class)
+        );
+
         return $this->render('tag/show.html.twig', ['tag' => $tag]);
     }
 
     /**
      * @Route("/{id}/edit", name="tag_edit", methods="GET|POST")
+     * @param Request $request
+     * @param Tag $tag
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function edit(Request $request, Tag $tag): Response
+    public function edit(Request $request, Tag $tag, LoggerInterface $logger): Response
     {
+        $logger->debug(
+            'Editing tag from given id.',
+            array('tag' => $tag->getLabel(), 'method' => 'edit', 'class' => self::class)
+        );
+
         $form = $this->createForm(TagType::class, $tag);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $logger->debug(
+                'Form is submitted and valid.',
+                array('tag' => $tag->getLabel(), 'method' => 'edit', 'class' => self::class)
+            );
+
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('tag_edit', ['id' => $tag->getId()]);
         }
+
+        $logger->debug(
+            'Form is not submitted. Rendering template.',
+            array('tag' => $tag->getLabel(), 'method' => 'edit', 'class' => self::class)
+        );
 
         return $this->render('tag/edit.html.twig', [
             'tag' => $tag,
@@ -76,14 +119,33 @@ class TagController extends Controller
 
     /**
      * @Route("/{id}", name="tag_delete", methods="DELETE")
+     * @param Request $request
+     * @param Tag $tag
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function delete(Request $request, Tag $tag): Response
+    public function delete(Request $request, Tag $tag, LoggerInterface $logger): Response
     {
+        $logger->debug(
+            'Deleting tag from given id.',
+            array('tag' => $tag->getLabel(), 'method' => 'delete', 'class' => self::class)
+        );
+
         if ($this->isCsrfTokenValid('delete'.$tag->getId(), $request->request->get('_token'))) {
+            $logger->debug(
+                'Form is submitted and valid.',
+                array('tag' => $tag->getLabel(), 'method' => 'delete', 'class' => self::class)
+            );
+
             $em = $this->getDoctrine()->getManager();
             $em->remove($tag);
             $em->flush();
         }
+
+        $logger->debug(
+            'Form is not submitted, rendering template.',
+            array('tag' => $tag->getLabel(), 'method' => 'delete', 'class' => self::class)
+        );
 
         return $this->redirectToRoute('tag_index');
     }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,22 +18,36 @@ class UserController extends Controller
 {
     /**
      * @Route("/", name="user_index", methods="GET")
+     * @param UserRepository $userRepository
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, LoggerInterface $logger): Response
     {
+        $logger->debug('Returning user entities.', array('method' => 'index', 'class' => self::class));
         return $this->render('user/index.html.twig', ['users' => $userRepository->findAll()]);
     }
 
     /**
      * @Route("/new", name="user_new", methods="GET|POST")
+     * @param Request $request
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function new(Request $request): Response
+    public function new(Request $request, LoggerInterface $logger): Response
     {
+        $logger->debug('Request for a new user entity.', array('method' => 'new', 'class' => self::class));
+
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $logger->debug(
+                'Form is submitted and valid.',
+                array('instagram id' => $user->getInstagramId(), 'method' => 'new', 'class' => self::class)
+            );
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
@@ -40,6 +55,7 @@ class UserController extends Controller
             return $this->redirectToRoute('user_index');
         }
 
+        $logger->debug('Form is not submitted. Rendering template.', array('method' => 'new', 'class' => self::class));
         return $this->render('user/new.html.twig', [
             'user' => $user,
             'form' => $form->createView(),
@@ -48,25 +64,52 @@ class UserController extends Controller
 
     /**
      * @Route("/{id}", name="user_show", methods="GET")
+     * @param User $user
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function show(User $user): Response
+    public function show(User $user, LoggerInterface $logger): Response
     {
+        $logger->debug(
+            'Showing user from given id.',
+            array('instagram id' => $user->getInstagramId(), 'method' => 'show', 'class' => self::class)
+        );
+
         return $this->render('user/show.html.twig', ['user' => $user]);
     }
 
     /**
      * @Route("/{id}/edit", name="user_edit", methods="GET|POST")
+     * @param Request $request
+     * @param User $user
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function edit(Request $request, User $user): Response
+    public function edit(Request $request, User $user, LoggerInterface $logger): Response
     {
+        $logger->debug(
+            'Editing user from given id.',
+            array('instagram id' => $user->getInstagramId(), 'method' => 'edit', 'class' => self::class)
+        );
+
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $logger->debug(
+                'Form is submitted and valid.',
+                array('instagram id' => $user->getInstagramId(), 'method' => 'edit', 'class' => self::class)
+            );
+
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('user_edit', ['id' => $user->getId()]);
         }
+
+        $logger->debug(
+            'Form is not submitted. Rendering template.',
+            array('instagram id' => $user->getInstagramId(), 'method' => 'edit', 'class' => self::class)
+        );
 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
@@ -76,14 +119,33 @@ class UserController extends Controller
 
     /**
      * @Route("/{id}", name="user_delete", methods="DELETE")
+     * @param Request $request
+     * @param User $user
+     * @param LoggerInterface $logger
+     * @return Response
      */
-    public function delete(Request $request, User $user): Response
+    public function delete(Request $request, User $user, LoggerInterface $logger): Response
     {
+        $logger->debug(
+            'Deleting user from given id.',
+            array('instagram id' => $user->getInstagramId(), 'method' => 'delete', 'class' => self::class)
+        );
+
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $logger->debug(
+                'Form is submitted and valid.',
+                array('instagram id' => $user->getInstagramId(), 'method' => 'delete', 'class' => self::class)
+            );
+
             $em = $this->getDoctrine()->getManager();
             $em->remove($user);
             $em->flush();
         }
+
+        $logger->debug(
+            'Form is not submitted, rendering template.',
+            array('instagram id' => $user->getInstagramId(), 'method' => 'delete', 'class' => self::class)
+        );
 
         return $this->redirectToRoute('user_index');
     }
